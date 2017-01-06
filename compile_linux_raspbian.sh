e4c2cbe108942cbaa3debbd140c4c4bd6500a869
notalentgeek <mikael.pratama@yahoo.com>
2017-01-06 04:41:10 +0100

    Compilation shell.

---
+++ compile_linux_raspbian.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo pyinstaller --paths="./src/cli" --paths="./src/collection_function" --paths="./src/config_and_database" --paths="./src/input" --paths="./src/other" --onefile pysoc.py
