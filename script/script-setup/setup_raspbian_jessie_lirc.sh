855c7d1a7d0da975321a6a520460054d1284022d
notalentgeek <mikael.pratama@yahoo.com>
2017-01-19 15:24:55 +0100

    Save.

--- script/script-setup/setup_raspbian_jessie_lirc.sh
+++ script/script-setup/setup_raspbian_jessie_lirc.sh
@@ -9,7 +9,7 @@ sudo /bin/sh -c 'printf "\n\nlirc_dev\nlirc_rpi gpio_in_pin=23 gpio_out_pin=22" 
 
 # Deleting `hardware.conf` and then re - create it back.
 sudo rm /etc/lirc/hardware.conf
-sudo /bin/sh -c 'printf "\n########################################################\n# /etc/lirc/hardware.conf\n#\n# Arguments which will be used when launching lircd\nLIRCD_ARGS=\"--uinput\"\n# Do not start lircmd even if there seems to be a good config file\n# START_LIRCMD=false\n# Do not start irexec, even if a good config file seems to exist.\n# START_IREXEC=false\n# Try to load appropriate kernel modules\nLOAD_MODULES=true\n# Run \"lircd --driver=help\" for a list of supported drivers.\nDRIVER=\"default\"\n# usually /dev/lirc0 is the correct setting for systems using udev\nDEVICE=\"/dev/lirc0\"\nMODULES=\"lirc_rpi\"\n# Default configuration files for your hardware if any\nLIRCD_CONF=\"\"\nLIRCMD_CONF=\"\"\n########################################################" >> /etc/lirc/hardware.conf'
+sudo /bin/sh -c 'printf "########################################################\n# /etc/lirc/hardware.conf\n#\n# Arguments which will be used when launching lircd\nLIRCD_ARGS=\"--uinput\"\n\n# Do not start lircmd even if there seems to be a good config file\n# START_LIRCMD=false\n\n# Do not start irexec, even if a good config file seems to exist.\n# START_IREXEC=false\n\n# Try to load appropriate kernel modules\nLOAD_MODULES=true\n\n# Run \"lircd --driver=help\" for a list of supported drivers.\nDRIVER=\"default\"\n# usually /dev/lirc0 is the correct setting for systems using udev\nDEVICE=\"/dev/lirc0\"\nMODULES=\"lirc_rpi\"\n\n# Default configuration files for your hardware if any\nLIRCD_CONF=\"\"\nLIRCMD_CONF=\"\"\n########################################################" >> /etc/lirc/hardware.conf'
 
 # Add line into boot configuration.
 sudo /bin/sh -c 'printf "\ndtoverlay=lirc-rpi,gpio_in_pin=23,gpio_out_pin=22" >> /boot/config.txt'
@@ -20,7 +20,7 @@ sudo /bin/sh -c 'printf "\n# Please make this file available to others\n# by sen
 
 # Add local and system wide `lircrc`.
 sudo /bin/sh -c 'printf "\nbegin\n    button = KEY_1\n    config = KEY_1\n    prog = pysoc\nend\nbegin\n    button = KEY_2\n    config = KEY_2\n    prog = pysoc\nend\nbegin\n    button = KEY_3\n    config = KEY_3\n    prog = pysoc\nend" >> /home/pi/.lircrc'
-sudo /bin/sh -c 'printf "\nbegin\n    button = KEY_1\n    config = KEY_1\n    prog = pysoc\nend\nbegin\n    button = KEY_2\n    config = KEY_2\n    prog = pysoc\nend\nbegin\n    button = KEY_3\n    config = KEY_3\n    prog = pysoc\nend\" >> /etc/lirc/lircrc'
+sudo /bin/sh -c 'printf "\nbegin\n    button = KEY_1\n    config = KEY_1\n    prog = pysoc\nend\nbegin\n    button = KEY_2\n    config = KEY_2\n    prog = pysoc\nend\nbegin\n    button = KEY_3\n    config = KEY_3\n    prog = pysoc\nend" >> /etc/lirc/lircrc'
 
 # Restart `lirc` service.
 sudo /etc/init.d/lirc stop
