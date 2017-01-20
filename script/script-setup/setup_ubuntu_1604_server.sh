81696853c7f0a7fa17256f17dcec4d3b96f01e63
notalentgeek <mikael.pratama@yahoo.com>
2017-01-20 11:49:24 +0100

    Save.

--- script/script-setup/setup_ubuntu_1604_server.sh
+++ script/script-setup/setup_ubuntu_1604_server.sh
@@ -1,15 +1,12 @@
 #!bin/bash
 
-adduser mikael
-usermod -aG sudo mikael
-su - mikael
-
-git clone https://github.com/notalentgeek/pysoc
-
-source /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
-wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install python3 python3-pip rethinkdb
-
-yes | sudo pip3 install -r ~/pysoc/req/requirement_ubuntu_1604_server.txt
-cd ~/pysoc
+source /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list &&
+wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add - &&
+sudo apt-get update &&
+yes | sudo apt-get install python3 python3-pip rethinkdb &&
+yes | sudo pip3 install -r ~/pysoc/req/requirement_ubuntu_1604_server.txt &&
+sudo cp /etc/rethinkdb/default.conf.sample /etc/rethinkdb/instances.d/instance1.conf &&
+sudo sed -ie "s/# bind=127.0.0.1/bind=all/g" /etc/rethinkdb/instances.d/instance1.conf &&
+sudo /bin/sh -c 'printf "\nsudo /etc/init.d/rethinkdb restart" >> ~/.bashrc' &&
+sudo /bin/sh -c 'printf "\npython3 -B ~/pysoc/pysoc_server.py "$(curl ipinfo.io/ip)" -o" >> ~/.bashrc' &&
+source ~/.bashrc
