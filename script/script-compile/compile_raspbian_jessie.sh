e0dcbc5997ecbcad772cb6d506cd1077a94bc819
notalentgeek <mikael.pratama@yahoo.com>
2017-01-20 10:36:53 +0100

    Adding better automation for compiling.

--- script/script-compile/compile_raspbian_jessie.sh
+++ script/script-compile/compile_raspbian_jessie.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo pyinstaller --paths="/home/pi/pysoc/src" --paths="/home/pi/pysoc/src/cli" --paths="/home/pi/pysoc/src/collection-function" --paths="/home/pi/pysoc/src/config-and-database" --paths="/home/pi/pysoc/src/input" --onefile /home/pi/pysoc/pysoc.py
-sudo chmod +x /home/pi/pysoc/dist/pysoc
-echo "PATH=$PATH:/home/pi/pysoc/dist" >> home/pi/.bash_profile
-export PATH=$PATH:/home/pi/pysoc/dist
-source home/pi/.bash_profile
-$SHELL
+
+sudo pyinstaller --paths="/home/pi/pysoc/src" --paths="/home/pi/pysoc/src/cli" --paths="/home/pi/pysoc/src/collection-function" --paths="/home/pi/pysoc/src/config-and-database" --paths="/home/pi/pysoc/src/input" --onefile /home/pi/pysoc/pysoc.py &&
+sudo chmod +x /home/pi/pysoc/dist/pysoc &&
+echo "PATH=$PATH:/home/pi/pysoc/dist" >> home/pi/.bash_profile &&
+export PATH=$PATH:/home/pi/pysoc/dist &&
+source home/pi/.bash_profile &&
--- script/script-compile/compile_ubuntu_1604.sh
+++ script/script-compile/compile_ubuntu_1604.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-pyinstaller --paths="./src" --paths="./src/cli" --paths="./src/collection-function" --paths="./src/config-and-database" --paths="./src/input" --onefile pysoc.py
-sudo chmod +x ~/pysoc/dist/pysoc
-echo "PATH=$PATH:~/pysoc/dist" >> ~/.bash_profile
-export PATH=$PATH:~/pysoc/dist
+
+pyinstaller --paths="./src" --paths="./src/cli" --paths="./src/collection-function" --paths="./src/config-and-database" --paths="./src/input" --onefile pysoc.py &&
+sudo chmod +x ~/pysoc/dist/pysoc &&
+echo "PATH=$PATH:~/pysoc/dist" >> ~/.bash_profile &&
+export PATH=$PATH:~/pysoc/dist &&
 source ~/.bashrc
-$SHELL
