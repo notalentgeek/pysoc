1f5685c77e301da2e948372e4db203d1f2b4e541
notalentgeek <mikael.pratama@yahoo.com>
2017-01-20 16:02:02 +0100

    Test.

--- static/main.js
+++ static/main.js
@@ -72,7 +72,7 @@ setInterval(function(){
 
                     DetermineDegreeTargetList(clientCircleList.length + 1);
                     for(var j = 0; j < clientCircleList.length; j ++){ clientCircleList[j].RotateAuto(); }
-                    new ClientCircle(clientTemporary, degreeTargetList[0]).RotateAuto();
+                    new ClientCircle(clientTemporary, degreeTargetList[0]);
 
                     console.log(clientTemporary);
 
