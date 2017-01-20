73a183a8899041deffedb4bd7987edef927d196d
notalentgeek <mikael.pratama@yahoo.com>
2017-01-20 14:04:40 +0100

    Adding check if webserver online or not.

--- script/script-setup/setup_ubuntu_1604_server.sh
+++ script/script-setup/setup_ubuntu_1604_server.sh
@@ -7,6 +7,6 @@ yes | sudo apt-get install python3 python3-pip rethinkdb &&
 yes | sudo pip3 install -r ~/pysoc/req/requirement_ubuntu_1604_server.txt &&
 sudo cp /etc/rethinkdb/default.conf.sample /etc/rethinkdb/instances.d/instance1.conf &&
 sudo sed -ie "s/# bind=127.0.0.1/bind=all/g" /etc/rethinkdb/instances.d/instance1.conf &&
-sudo /bin/sh -c 'printf "\nsudo /etc/init.d/rethinkdb restart" >> ~/.bashrc' &&
-sudo /bin/sh -c 'printf "\npython3 -B ~/pysoc/pysoc_server.py "$(curl ipinfo.io/ip)" -o" >> ~/.bashrc' &&
-source ~/.bashrc
+printf "\nif netstat -an | grep -w \"29015\"; then\n    PORT_USED_DB=1\nelse\n    PORT_USED_DB=0\nfi\nif netstat -an | grep -w \"5000\"; then\n    PORT_USED_WEB=1\nelse\n    PORT_USED_WEB=0\nfi\nif [ \"\$PORT_USED_DB\" = \"0\" ]; then\n    sudo /etc/init.d/rethinkdb restart\nfi\nif [ \"\$PORT_USED_WEB\" = \"0\" ]; then\n    python3 -B ~/pysoc/pysoc_server.py \"$(curl ipinfo.io/ip)\" -o\nfi" >> ~/.bashrc &&
+sudo /etc/init.d/rethinkdb restart &&
+python3 -B ~/pysoc/pysoc_server.py "$(curl ipinfo.io/ip)" -o
