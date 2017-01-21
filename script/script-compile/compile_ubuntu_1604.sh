7ce5dad7ccba046a3a3c5e327c225c1f6ce9f423
notalentgeek <mikael.pratama@yahoo.com>
2017-01-21 18:02:26 +0100

    Added into system wide bashrc.

--- script/script-compile/compile_ubuntu_1604.sh
+++ script/script-compile/compile_ubuntu_1604.sh
@@ -5,4 +5,4 @@ rm -r build dist __pycache__ log ||
 pyinstaller --paths="./src" --paths="./src/cli" --paths="./src/collection-function" --paths="./src/config-and-database" --paths="./src/input" --onefile pysoc.py &&
 sudo chmod +x ~/pysoc/dist/pysoc &&
 grep -q -F "PATH=\$PATH:~/pysoc/dist" ~/.bashrc || printf "PATH=\$PATH:~/pysoc/dist" >> ~/.bashrc
-export PATH=$PATH:~/pysoc/dist
+ln -s ~/pysoc/dist/pysoc /usr/local/bin
