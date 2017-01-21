8121b13067c97b2b8b98c71e7e775285fa6ad6b5
notalentgeek <mikael.pratama@yahoo.com>
2017-01-21 06:50:42 +0100

    Change in requirement files.

--- script/script-setup/setup_ubuntu_1604_server.sh
+++ script/script-setup/setup_ubuntu_1604_server.sh
@@ -8,7 +8,7 @@ yes | sudo apt-get install python3 python3-pip rethinkdb &&
 sudo cp /etc/rethinkdb/default.conf.sample /etc/rethinkdb/instances.d/instance1.conf &&
 sudo sed -ie "s/# bind=127.0.0.1/bind=all/g" /etc/rethinkdb/instances.d/instance1.conf &&
 
-yes | sudo pip3 install -r ~/pysoc/req/requirement_ubuntu_1604_server.txt &&
+yes | sudo pip3 install -r ~/pysoc/req/req_ubuntu_1604_server.txt &&
 
 printf "\nif netstat -an | grep -w \"29015\"; then\n    PORT_USED_DB=1\nelse\n    PORT_USED_DB=0\nfi\nif netstat -an | grep -w \"5000\"; then\n    PORT_USED_WEB=1\nelse\n    PORT_USED_WEB=0\nfi\nif [ \"\$PORT_USED_DB\" = \"0\" ]; then\n    sudo /etc/init.d/rethinkdb restart\nfi\nif [ \"\$PORT_USED_WEB\" = \"0\" ]; then\n    python3 -B ~/pysoc/pysoc_server.py 127.0.0.1 -o\nfi" >> ~/.bashrc &&
 
