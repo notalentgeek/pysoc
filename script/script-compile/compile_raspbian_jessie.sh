9c83969625dcfca4c69a0214670265e37c9c46d7
notalentgeek <mikael.pratama@yahoo.com>
2017-01-21 16:13:25 +0100

    Fixing compiler script.

--- script/script-compile/compile_raspbian_jessie.sh
+++ script/script-compile/compile_raspbian_jessie.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+cd /home/pi/pysoc
 sudo rm -r /home/pi/pysoc/build /home/pi/pysoc/dist /home/pi/pysoc/__pycache__ /home/pi/pysoc/log ||
 sudo pyinstaller --paths="/home/pi/pysoc/src" --paths="/home/pi/pysoc/src/cli" --paths="/home/pi/pysoc/src/collection-function" --paths="/home/pi/pysoc/src/config-and-database" --paths="/home/pi/pysoc/src/input" --onefile /home/pi/pysoc/pysoc.py &&
 sudo chmod +x /home/pi/pysoc/dist/pysoc &&
-sudo sed -ie "s#\nPATH=\$PATH:~/pysoc/dist##g" /home/pi/.bashrc &&
-sudo /bin/sh -c 'printf "\nPATH=\$PATH:~/pysoc/dist" >> /home/pi/.bashrc' &&
+sudo sed -ie "s#PATH=$PATH:/home/pi/pysoc/dist##g" /home/pi/.bashrc &&
+sudo /bin/sh -c 'printf "\nPATH=\$PATH:/home/pi/pysoc/dist" >> /home/pi/.bashrc' &&
 export PATH=$PATH:~/pysoc/dist
--- script/script-compile/compile_ubuntu_1604.sh
+++ script/script-compile/compile_ubuntu_1604.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+cd ~/pysoc
 rm -r build dist __pycache__ log ||
 pyinstaller --paths="./src" --paths="./src/cli" --paths="./src/collection-function" --paths="./src/config-and-database" --paths="./src/input" --onefile pysoc.py &&
 sudo chmod +x ~/pysoc/dist/pysoc &&
-sed -ie "s#\nPATH=\$PATH:~/pysoc/dist##g" ~/.bashrc &&
+sed -ie "s#PATH=PATH:~/pysoc/dist##g" ~/.bashrc &&
 printf "\nPATH=\$PATH:~/pysoc/dist" >> ~/.bashrc &&
 export PATH=$PATH:~/pysoc/dist
