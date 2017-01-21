84837a39e38ea0beac698ba77ac01990aa35823a
notalentgeek <mikael.pratama@yahoo.com>
2017-01-21 17:21:34 +0100

    Export Pysoc.

--- script/script-compile/compile_ubuntu_1604.sh
+++ script/script-compile/compile_ubuntu_1604.sh
@@ -4,6 +4,5 @@ cd ~/pysoc
 rm -r build dist __pycache__ log ||
 pyinstaller --paths="./src" --paths="./src/cli" --paths="./src/collection-function" --paths="./src/config-and-database" --paths="./src/input" --onefile pysoc.py &&
 sudo chmod +x ~/pysoc/dist/pysoc &&
-sed -ie "s#PATH=PATH:~/pysoc/dist##g" ~/.bashrc &&
-printf "\nPATH=\$PATH:~/pysoc/dist" >> ~/.bashrc &&
+grep -q -F "PATH=\$PATH:~/pysoc/dist" ~/.bashrc || printf "PATH=\$PATH:~/pysoc/dist" >> ~/.bashrc
 export PATH=$PATH:~/pysoc/dist
