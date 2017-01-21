46758debf33a0d4d95a998abce664b7b9b9d8c8b
notalentgeek <mikael.pratama@yahoo.com>
2017-01-22 00:58:32 +0100

    Fixing test.

--- static/main.js
+++ static/main.js
@@ -32,199 +32,52 @@ setInterval(function(){
         if(receivedData !== null && receivedData !== undefined){
 
             // Set all `clientList[x].online` into `false`.
+            // As well as to set `clientList[x].latestIRCodeClientNameRaw`
+            // to null.
             for(var i = 0; i < clientList.length; i ++){
 
                 clientList[i].latestIRCodeClientNameRaw = null;
-                clientList[i].online = false;
+                clientList[i].online                    = false;
 
             }
 
             //console.log(receivedData);
 
+            // Check all received data.
             for(var i = 0; i < receivedData.length; i ++){
 
-                //console.log(receivedData[i]);
-                //console.log(typeof(receivedData[i]));
+                // Temporary variable for current received
+                // client.
+                var clientReceived;
 
-                // Check if there is a client object with this
-                // name in the `clientList` if not then make
-                // a new one.
-                var clientName = String(receivedData[i]["client_name"]);
-                var clientTemporary;
-                //console.log(clientName);
+                //console.log(receivedData[i])
+                //console.log(typeof(receivedData[i]))
+
+                // Check if there is a client list with the same
+                // name in the `receivedData[i]`.
+                var clientReceivedName = String();
                 for(var j = 0; j < clientList.length; j ++){
 
-                    if(clientList[j].name == clientName){
+                    // In this case there is already a client listed
+                    // in the clientList array. This means that this
+                    // received client used to be online in this
+                    // local session.
+                    if(clientList[j].name == clientReceivedName){
 
-                        clientTemporary = clientList[j];
-                        clientTemporary.online = true;
+                        // Assign the found client into
+                        // `clientReceived`. And mark its
+                        // `online` status into `true`.
+                        clientReceived = clientList[j];
+                        clientReceived.online = true;
 
-                        if(clientTemporary.clientCircle === null || clientTemporary.clientCircle === undefined){
-
-                            //DetermineDegreeTargetList(clientCircleList.length + 1);
-                            //for(var j = 0; j < clientCircleList.length; j ++){ if(clientCircleList[j].client.online){ clientCircleList[j].RotateAuto(); } }
-                            new ClientCircle(clientTemporary, degreeTargetList[0]);
-
-                        }
-
-                        break;
-
-                    }
-
-                }
-                if(clientTemporary === null || clientTemporary === undefined){
-
-                    //DetermineDegreeTargetList(clientCircleList.length + 1);
-                    //for(var j = 0; j < clientCircleList.length; j ++){ if(clientCircleList[j].client.online){ clientCircleList[j].RotateAuto(); } }
-                    clientTemporary = new Client(clientName, false);
-                    clientTemporary.online = true;
-
-                    console.log(degreeTargetList);
-                    console.log(degreeTargetList.length);
-
-                    var newCircle = new ClientCircle(clientTemporary, degreeTargetList[0]);
-                    newCircle.RotateAuto();
-
-                    console.log(clientTemporary.name);
-                    console.log(newCircle);
-                    console.log(newCircle.cX);
-
-                }
-
-                for(var j = 0; j < clientList.length; j ++){
-
-                    if(!clientList[j].online && clientList[j].clientCircle !== null && clientList[j].clientCircle !== undefined){
-
-
-                        clientList[j].AddLatest();
-                        clientList[j].clientCircle.willBeDeleted = true;
-
-                    }
-
-                    if(clientList[j].online){
-
-                        var amountFace = Number(receivedData[i]["faces"]);
-                        clientList[j].latestAmountFace = amountFace;
-                        //console.log(amountFace);
-
-                        var irCodeClientNameRaw = (String(receivedData[i]["ir_code"]) == "undefined") ? null : String(receivedData[i]["ir_code"]);
-                        clientList[j].latestIRCodeClientNameRaw = irCodeClientNameRaw;
-                        //console.log(irCodeClientNameRaw);
-
-                        var amountPitch = Number(receivedData[i]["pitch"]);
-                        clientList[j].latestAmountPitch = amountPitch;
-                        //console.log(amountPitch);
-
-                        var amountVolume = Number(receivedData[i]["volume"]);
-                        clientList[j].latestAmountVolume = amountVolume;
-                        //console.log(amountVolume);
-
-                    }
-
-                }
-
-                //console.log(clientList);
-
-            }
-            for(var i = 0; i < clientList.length; i ++){
-
-                if(clientList[i].online && clientList[i].clientCircle !== null && clientList[i].clientCircle !== undefined){
-
-
-                    clientList[i].AddLatest();
-
-                }
-
-                if(clientList[i].latestIRCodeClientNameRaw !== null && clientList[i].latestIRCodeClientNameRaw !== undefined){
-
-                    //console.log(clientList[i].latestIRCodeClientNameRaw);
-                    //console.log(typeof(clientList[i].latestIRCodeClientNameRaw));
-
-                    var latestIRCodeClientNameRawTemporary = clientList[i].latestIRCodeClientNameRaw.split(",");
-                    for(var j = 0; j < latestIRCodeClientNameRawTemporary.length; j ++){
-
-                        clientList[i].latestIRCodeClientName.push(latestIRCodeClientNameRawTemporary[j]);
-
-                        var clientTemporary;
-                        for(var k = 0; k < clientList.length; k ++){
-
-                            if(clientList[k].name == latestIRCodeClientNameRawTemporary[j]){
-
-                                clientTemporary = clientList[k];
-                                break;
-
-                            }
-
-                        }
-
-                        clientList[i].latestIRCodeClientCircle.push(clientTemporary.clientCircle);
-
-                    }
-
-                }
-
-            }
-
-            //DetermineDegreeTargetList(clientCircleList.length);
-            //for(var j = 0; j < clientCircleList.length; j ++){ if(clientCircleList[j].client.online){ clientCircleList[j].RotateAuto(); } }
-
-            d3.selectAll(".real-circle").remove();
-            d3.selectAll(".real-line").remove();
-
-            for(var i = 0; i < clientList.length; i ++){
-
-                for(var j = 0; j < clientList[i].latestIRCodeClientCircle.length; j ++){
-
-                    if(clientList[i].clientCircle !== null && clientList[i].clientCircle !== undefined){
-
+                        // After that check if the current
+                        // inspected client has its `clientCircle`.
                         if(
-                            (clientList[i].clientCircle.degreeCurrent == clientList[i].clientCircle.degreeTarget) &&
-                            (clientList[i].latestIRCodeClientCircle[j].degreeCurrent == clientList[i].latestIRCodeClientCircle[j].degreeTarget)
+                            clientReceived.clientCircle === null ||
+                            clientReceived.clientCircle === undefined
                         ){
 
-                            var cX1 = Number(clientList[i].clientCircle.circle.attr("cx"));
-                            var cY1 = Number(clientList[i].clientCircle.circle.attr("cy"));
-                            var cX2 = Number(clientList[i].latestIRCodeClientCircle[j].circle.attr("cx"));
-                            var cY2 = Number(clientList[i].latestIRCodeClientCircle[j].circle.attr("cy"));
-                            var r1 = clientList[i].clientCircle.radius;
-                            var r2 = clientList[i].latestIRCodeClientCircle[j].radius;
-
-                            var radian = Math.atan2(cY2 - cY1, cX2 - cX1);
-
-                            var x1 = cX1 + (r1 * Math.cos(radian));
-                            var y1 = cY1 + (r1 * Math.sin(radian));
-                            var x2 = cX2 - (r2 * Math.cos(radian));
-                            var y2 = cY2 - (r2 * Math.sin(radian));
-
-                            //console.log("test");
-
-                            d3SVG.append("line")
-                                .attr("class", "real-line")
-                                .attr("x1", x1)
-                                .attr("y1", y1)
-                                .attr("x2", x2)
-                                .attr("y2", y2)
-                                .attr(
-                                    "transform",
-                                    "translate(" + d3DimensionTranslate.x + ", " + d3DimensionTranslate.y + ")"
-                                )
-                                .style("opacity", 0.5)
-                                .style("stroke", clientList[i].clientCircleColor)
-                                .style("stroke-width", 5);
-
-                            d3SVG.append("circle")
-                                .attr("class", "real-circle")
-                                .attr("cx", x2)
-                                .attr("cy", y2)
-                                .attr("r", 5)
-                                .attr(
-                                    "transform",
-                                    "translate(" + d3DimensionTranslate.x + ", " + d3DimensionTranslate.y + ")"
-                                )
-                                .style("fill", clientList[i].clientCircleColor)
-                                .style("stroke", "no-stroke");
-
-                            //this.latestIRCodeClientLine.push(line);
+                            new ClientCircle(clientReceived, 0);
 
                         }
 
@@ -232,12 +85,19 @@ setInterval(function(){
 
                 }
 
-            }
-            DetermineDegreeTargetList(clientCircleList.length);
-            //for(var j = 0; j < clientList.length; j ++){ if(clientList[j].online){ clientList[j].clientCircle.RotateAuto(); } }
-            for(var j = 0; j < clientCircleList.length; j ++){ if(clientCircleList[j].client.online){ clientCircleList[j].RotateAuto(); } }
+                // This will happen if the client is completely new in
+                // this local session.
+                if(
+                    clientTemporary === null ||
+                    clientTemporary === undefined
+                ){
 
-            receivedData = null;
+                    clientTemporary = new Client(clientReceivedName, false);
+                    new ClientCircle(clientTemporary, 0);
+
+                }
+
+            }
 
         }
 
