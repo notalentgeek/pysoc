7b7a91b410abf754b39cd3cf011ee48ca1d06c80
notalentgeek <mikael.pratama@yahoo.com>
2017-01-21 16:01:24 +0100

    Compilation setup.

--- script/script-compile/compile_raspbian_jessie.sh
+++ script/script-compile/compile_raspbian_jessie.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+sudo rm -r /home/pi/pysoc/build /home/pi/pysoc/dist /home/pi/pysoc/__pycache__ /home/pi/pysoc/log ||
 sudo pyinstaller --paths="/home/pi/pysoc/src" --paths="/home/pi/pysoc/src/cli" --paths="/home/pi/pysoc/src/collection-function" --paths="/home/pi/pysoc/src/config-and-database" --paths="/home/pi/pysoc/src/input" --onefile /home/pi/pysoc/pysoc.py &&
 sudo chmod +x /home/pi/pysoc/dist/pysoc &&
-echo "PATH=$PATH:/home/pi/pysoc/dist" >> home/pi/.bash_profile &&
-export PATH=$PATH:/home/pi/pysoc/dist &&
-source home/pi/.bash_profile
+sudo sed -ie "s#\nPATH=\$PATH:~/pysoc/dist##g" ~/.bashrc &&
+sudo /bin/sh -c 'printf "\nPATH=\$PATH:~/pysoc/dist" >> ~/.bashrc' &&
+export PATH=$PATH:~/pysoc/dist &&
+source ~/.bashrc
--- script/script-compile/compile_ubuntu_1604.sh
+++ script/script-compile/compile_ubuntu_1604.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+rm -r build dist __pycache__ log ||
 pyinstaller --paths="./src" --paths="./src/cli" --paths="./src/collection-function" --paths="./src/config-and-database" --paths="./src/input" --onefile pysoc.py &&
 sudo chmod +x ~/pysoc/dist/pysoc &&
-echo "PATH=$PATH:~/pysoc/dist" >> ~/.bash_profile &&
+sed -ie "s#\nPATH=\$PATH:~/pysoc/dist##g" ~/.bashrc &&
+printf "\nPATH=\$PATH:~/pysoc/dist" >> ~/.bashrc &&
 export PATH=$PATH:~/pysoc/dist &&
 source ~/.bashrc
