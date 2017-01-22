22ceaacd18eb6bb4952d443aec236be80563c937
notalentgeek <mikael.pratama@yahoo.com>
2017-01-22 06:10:09 +0100

    Remove wait().

--- static/client/client-circle/client_circle.js
+++ static/client/client-circle/client_circle.js
@@ -236,14 +236,6 @@ function ClientCircleAnimation(){
                 clientCircleTemp.client = null;
                 delete clientCircleTemp;
 
-                DetermineDegreeTargetList(clientCircleList.length);
-                for(var j = 0; j < clientCircleList.length; j ++){
-
-                    clientCircleList[j].degreeSaved = clientCircleList[j].degreeCurrent;
-                    clientCircleList[j].RotateAuto();
-
-                }
-
             }
 
         }
