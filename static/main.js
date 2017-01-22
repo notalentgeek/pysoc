73b05bb623fb10d287f6350fe972b44b1cfcc5a0
notalentgeek <mikael.pratama@yahoo.com>
2017-01-22 05:55:42 +0100

    Remove wait().

--- static/main.js
+++ static/main.js
@@ -23,7 +23,7 @@ setInterval(function(){
     //console.log("1 second just passed");
     //console.log(focus());
     //console.log(clientCircleList);
-    console.log(receivedData);
+    //console.log(receivedData);
     //if(receivedData !== null && receivedData !== undefined){ console.log(receivedData[0]["pitch"] + " " + receivedData[0]["volume"]); }
 
     socket.emit("latestInputRequest");
@@ -199,7 +199,7 @@ setInterval(function(){
                 if(
                     !clientList[i].online &&
                     (
-                        clientList[i].clientCircle !== null ||
+                        clientList[i].clientCircle !== null &&
                         clientList[i].clientCircle !== undefined
                     )
                 ){
