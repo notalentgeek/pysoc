31954bf41312540443dd884d31b8a835a09014ce
notalentgeek <mikael.pratama@yahoo.com>
2017-01-27 17:08:05 +0100

    This is complete I think.

--- script/script-setup/setup_ubuntu_1604_config_mikael_pc.sh
+++ script/script-setup/setup_ubuntu_1604_config_mikael_pc.sh
@@ -11,6 +11,7 @@ sudo chmod +x ~/pysoc/script/script-setup/*
 yes | sudo pip3 -r ~/pysoc/req/req_ubuntu_1604.sh
 sudo ~/pysoc/script/script-compile/compile_ubuntu_1604.sh &&
 
+cp ~/pysoc/premade-config/config_mikael_pc.ini /bin/config.ini &&
 cp ~/pysoc/premade-config/config_mikael_pc.ini ~/config.ini &&
 cp ~/pysoc/premade-config/config_mikael_pc.ini ~/pysoc/config.ini &&
 cp ~/pysoc/premade-config/config_mikael_pc.ini ~/pysoc/dist/config.ini &&
