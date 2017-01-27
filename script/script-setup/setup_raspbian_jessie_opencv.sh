275d24cab0fbdf1f88ecb2e6acb6125064e9528c
notalentgeek <mikael.pratama@yahoo.com>
2017-01-27 12:33:29 +0100

    OpenCV installation for Ubuntu.

--- script/script-setup/setup_raspbian_jessie_opencv.sh
+++ script/script-setup/setup_raspbian_jessie_opencv.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-yes | sudo apt-get purge wolfram-engine
-yes | sudo apt-get install build-essential cmake gfortran libatlas-base-dev libavcodec-dev libavformat-dev libgtk2.0-dev libjasper-dev libjpeg-dev libpng12-dev libswscale-dev libtiff5-dev libv4l-dev libx264-dev libxvidcore-dev pkg-config python2.7-dev python3-dev
-cd /home/pi
-wget -O opencv.zip https://github.com/Itseez/opencv/archive/3.1.0.zip
-unzip opencv.zip
-wget -O opencv_contrib.zip https://github.com/Itseez/opencv_contrib/archive/3.1.0.zip
-unzip opencv_contrib.zip
-cd /home/pi/opencv-3.1.0/
-mkdir build
-cd build
+yes | sudo apt-get purge wolfram-engine &&
+yes | sudo apt-get install build-essential cmake gfortran libatlas-base-dev libavcodec-dev libavformat-dev libgtk2.0-dev libjasper-dev libjpeg-dev libpng12-dev libswscale-dev libtiff5-dev libv4l-dev libx264-dev libxvidcore-dev pkg-config python2.7-dev python3-dev &&
+cd /home/pi &&
+wget -O opencv.zip https://github.com/Itseez/opencv/archive/3.1.0.zip &&
+unzip opencv.zip &&
+wget -O opencv_contrib.zip https://github.com/Itseez/opencv_contrib/archive/3.1.0.zip &&
+unzip opencv_contrib.zip &&
+cd /home/pi/opencv-3.1.0/ &&
+mkdir build &&
+cd build &&
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
     -D CMAKE_INSTALL_PREFIX=/usr/local \
     -D INSTALL_PYTHON_EXAMPLES=ON \
     -D OPENCV_EXTRA_MODULES_PATH=/home/pi/opencv_contrib-3.1.0/modules \
-    -D BUILD_EXAMPLES=ON ..
-make
-sudo make install
+    -D BUILD_EXAMPLES=ON .. &&
+make &&
+sudo make install &&
 sudo ldconfig
--- script/script-setup/setup_ubuntu_1604_opencv.sh
+++ script/script-setup/setup_ubuntu_1604_opencv.sh
@@ -1,2 +1,22 @@
 #!bin/bash
+
+yes | sudo apt-get install build-essential cmake gfortran libatlas-base-dev libavcodec-dev libavformat-dev libgtk-3-dev libjasper-dev libjpeg8-dev libpng12-dev libswscale-dev libtiff5-dev libv4l-dev libx264-dev libxvidcore-dev pkg-config python2.7-dev python3.5-dev &&
+cd ~ &&
+wget -O opencv.zip https://github.com/Itseez/opencv/archive/3.1.0.zip &&
+unzip opencv.zip &&
+wget -O opencv_contrib.zip https://github.com/Itseez/opencv_contrib/archive/3.1.0.zip &&
+unzip opencv_contrib.zip &&
+cd ~/opencv-3.1.0/ &&
+mkdir build &&
+cd build &&
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D INSTALL_PYTHON_EXAMPLES=ON \
+    -D INSTALL_C_EXAMPLES=OFF \
+    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.1.0/modules \
+    -D PYTHON_EXECUTABLE=~/.virtualenvs/cv/bin/python \
+    -D BUILD_EXAMPLES=ON .. &&
+make &&
+sudo make install &&
+sudo ldconfig &&
 ln -s /usr/local/lib/python3.5/dist-packages/cv2.cpython-35m-x86_64-linux-gnu.so ~/pysoc/cv2.so
