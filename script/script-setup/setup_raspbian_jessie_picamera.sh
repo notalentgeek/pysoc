f7a3f3435d0ff8d6a12c82b3c6ede203f2a5a75b
notalentgeek <mikael.pratama@yahoo.com>
2017-01-27 11:41:38 +0100

    Fixing printf.

--- script/script-setup/setup_raspbian_jessie_picamera.sh
+++ script/script-setup/setup_raspbian_jessie_picamera.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo sed -ie "/start_x=0/d" /boot/config.txt
-sudo grep -q -F "start_x=1" /boot/config.txt || sudo /bin/sh -c 'printf "start_x=1" >> /boot/config.txt'
-sudo grep -q -F "gpu_mem=128" /boot/config.txt || sudo /bin/sh -c 'printf "gpu_mem=128" >> /boot/config.txt'
+sudo grep -q -F "start_x=1" /boot/config.txt || sudo /bin/sh -c 'printf "\nstart_x=1" >> /boot/config.txt'
+sudo grep -q -F "gpu_mem=128" /boot/config.txt || sudo /bin/sh -c 'printf "\ngpu_mem=128" >> /boot/config.txt'
